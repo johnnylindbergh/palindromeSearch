24ba634c70ea2fd3b91183e0e4341203cb84d898
johnnylindbergh <johnnyl8@embarqmail.com>
2020-03-23 21:10:57 -0400

    palindrome search prototype

---
+++ palindromeSearch.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import string
+from nltk.tokenize import RegexpTokenizer
+
+
+def palindrome(s):
+	if (len(s)<=1):
+		return True
+	if (s[0] != s[-1]):
+		return False
+	else:
+		return palindrome(s[1:len(s)-1])
+
+def lower(corpus):
+	for word in range(0,len(corpus)):
+		lowerWord = ""
+		for char in range(0, len(corpus[word])):
+			lowerWord += (corpus[word][char].lower())
+		corpus[word] = lowerWord
+	return corpus
+
+def cleanCorpus(corpus):
+	corpus = corpus.replace("'", "")
+	corpus = corpus.replace("\xe2", "")
+	corpus = corpus.replace("_", "")
+	tokenizer = RegexpTokenizer(r'\w+')
+	corpus  = tokenizer.tokenize(corpus)
+	corpus = lower(corpus);
+	return corpus
+
+
+def findPalindromes(corpus, minPalindromeWordLength, maxPalindromeWordLength, palindromeDict):
+	for palindromeWordLength in range(minPalindromeWordLength,maxPalindromeWordLength+1):
+		for w in range(0, len(corpus)-palindromeWordLength+1):
+		 	pal = []
+		 	for ngram in range(0,palindromeWordLength):
+		 		pal.append(corpus[w+ngram])
+		 	concat = ""
+		 	concatWithSpaces = ""
+			for word in pal:
+				concat = concat + word
+				concatWithSpaces = concatWithSpaces + " " + word
+			if (len(concat)> 1):
+				if palindrome(concat):
+					if (concatWithSpaces in palindromeDict):
+						palindromeDict[concatWithSpaces] = palindromeDict[concatWithSpaces]+  1;
+					else:
+						palindromeDict[concatWithSpaces] = 1;
+
+palindromeDict = dict()
+for corpusNumber in range(1,2):
+
+	corpusName = "corpus" + str(corpusNumber) + ".txt"
+	f = open(corpusName,"r") 
+	corpus =  f.read()
+	corpus = cleanCorpus(corpus)
+	findPalindromes(corpus, 2, 7, palindromeDict);
+for i in palindromeDict:
+	print i
+
+print palindromeDict
